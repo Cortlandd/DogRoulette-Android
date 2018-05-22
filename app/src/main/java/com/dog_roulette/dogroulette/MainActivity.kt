9fdb0daf0fcc8b8f6c53d33edd46c641bf2d6207
Cortland Walker <cortland1568@gmail.com>
2018-05-22 17:21:15 -0500

    Setup random search query list, and player.

--- app/src/main/java/com/dog_roulette/dogroulette/MainActivity.kt
+++ app/src/main/java/com/dog_roulette/dogroulette/MainActivity.kt
@@ -3,9 +3,6 @@ package com.dog_roulette.dogroulette
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
-import com.google.api.services.youtube.YouTube
-import com.google.api.services.youtube.YouTubeRequest;
 
 import khttp.get
 import android.os.StrictMode
@@ -15,32 +12,40 @@ import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerList
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayer
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerInitListener
 import com.pierfrancescosoffritti.youtubeplayer.player.YouTubePlayerView
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    val API_KEY: String = "AIzaSyCWDXs_WwDOK3XQu8MPUx9aSdfJ8KOPfRE"
-
     fun search_youtube(query_params: String = ""): String? {
+
+        // Get reference to API Key
+        val API_KEY: String = getString(R.string.API_KEY)
 
         // TODO: Figure out a way to randomize search results
 
+        // API url to connect to
         val url = "https://www.googleapis.com/youtube/v3/search"
-        var query: String = "dog videos" + " "
 
+        // Video searches. Do this to minimize people seeing the same videos
+        var query = arrayListOf<String>("dog videos", "adorable dogs", "funny dogs", "nice dogs")
+
+        // Pick a random string from teh query array
+        var word: String = query[Random().nextInt(query.size)]
+
+        // If a user doesn;t enter text in the filter bar, use the default query
         if (query_params != "") {
-            query += query_params
+            word += query_params
         }
-        Log.i("TAG", query)
+        // Show me what what searched
+        Log.i("TAG", word)
 
-        // Store search params results here
-        //var results_list = listOf<Any>()
-        //var results_list = ""
+        // Store search params results inside JSON Object
         var results_list = JSONObject()
 
         // Parameters/Header info with video search
         val search_params = mapOf(
-                "q" to query,
+                "q" to word,
                 "part" to "id, snippet",
                 "start_index" to (50 - 49).toString(),
                 "safeSearch" to "none",
@@ -65,7 +70,6 @@ class MainActivity : AppCompatActivity() {
             results_list = rez.getJSONObject(0).getJSONObject("id")
             println(results_list["videoId"].toString())
         }
-
         return results_list["videoId"].toString()
     }
 
