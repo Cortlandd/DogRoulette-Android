cbca5f384422168bfbc8b77d40397f2cf5ea3f8e
Cortland Walker <cortland1568@gmail.com>
2018-05-28 23:28:06 -0500

    Setup Splash Screen

---
+++ app/src/main/java/com/dog_roulette/dogroulette/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.dog_roulette.dogroulette
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
